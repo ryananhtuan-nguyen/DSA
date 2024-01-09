eb50db7add49604d5b95510857ca1b852150e839
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-09 14:54:09 +1300

    array

---
+++ array.js
@@ -0,0 +1,9 @@
+const newList = [1, 2, 3];
+/**
+ * python:
+ * new_list = [1,2,3]
+ * result = new_list[0]
+ * arrays in python store pointers to the values, not the values themself
+ */
+
+const result = newList[0];
